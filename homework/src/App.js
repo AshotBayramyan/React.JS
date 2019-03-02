d1236203f56a7b77286f1e394f45252ddaa3dab9
Hasmik Poghosyan <hasmikpoghosyan@Hasmiks-MacBook-Pro.local>
2019-03-02 11:47:25 +0400

    x

---
+++ homework/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Project from "./components/index";
+
+class App extends Component {
+  render() {
+    return (
+        <Project/>
+    );
+    }
+}
+
+export default App;
+
---
+++ homework/src/import.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Import extends Component {
+    render() {
+      return (
+      <h1>This is the imported component</h1>
+      );
+    }
+  }
+  
+  export default Import;
