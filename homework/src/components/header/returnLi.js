9a1761109caad31f31c1eadd873cfa923fad7691
AshotBayramyan <ashotbayramyan@gmail.com>
2019-03-12 18:59:16 +0400

    ndgbntyeh

--- homework/src/components/header/returnLi.js
+++ homework/src/components/header/returnLi.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-// import React, {Component} from 'react';
-
-// class Li extends Component{
-
-//     render(){
-//         return(
-//         <li>{this.props.name} {this.props.text} </li>
-//         );
-//     }
-// }
-
-
-// export default Li;
-=======
 import React, {Component} from 'react';
 
 class Li extends Component{
@@ -25,4 +10,3 @@ class Li extends Component{
 }
 
 export default Li;
->>>>>>> d1236203f56a7b77286f1e394f45252ddaa3dab9
--- homework/src/components/index.js
+++ homework/src/components/index.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import React, {Component} from "react";
-// import Header from "./header/index";
-// import Content from "./content";
-
-class Project extends Component{
-    render(){
-        return(
-
-            <div className="cube"></div>
-
-
-            // <div className="container">
-            //     {/* <Header /> */}
-            //     {/* <Content />  */}
-            // </div>
-        );
-    }
-}
-
-
-=======
 import React, {Component} from "react";
 import Header from "./header/index";
 import Content from "./content";
@@ -35,5 +13,4 @@ class Project extends Component{
     }
 }
 
->>>>>>> d1236203f56a7b77286f1e394f45252ddaa3dab9
 export default Project;
